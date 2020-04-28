55895eba7c02a2b2e434c077ffd699022bf3b5cc
Steven Croft <steven.croft@optimizely.com>
2020-04-28 15:45:49 +1000

    Initial draft including Optimizely library

--- app/src/main/java/com/appsflyer/androidsampleapp/MainActivity.java
+++ app/src/main/java/com/appsflyer/androidsampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.appsflyer.androidsampleapp;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +12,17 @@ import com.appsflyer.AFInAppEventParameterName;
 import com.appsflyer.AFInAppEventType;
 import com.appsflyer.AppsFlyerLib;
 
+import com.optimizely.ab.android.sdk.OptimizelyManager;
+import com.optimizely.ab.android.sdk.OptimizelyClient;
+import com.optimizely.ab.android.sdk.OptimizelyStartListener;
+import com.optimizely.ab.config.Variation;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
     final Handler handler = new Handler();
 
@@ -25,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SetInstallDataText();
+
+        //Async Initialisation
+        OptimizelyManager optimizelyManager;
+        OptimizelyClient optimizelyClient;
+        Variation variation;
+        String userId = "user123";
+
+        optimizelyManager =  OptimizelyManager.builder()
+                .withSDKKey("3inoLTNUySErvUi4ihoX1F")
+                .withDatafileDownloadInterval(TimeUnit.MINUTES.toSeconds(15))
+                .build(this);
+        optimizelyManager.initialize(this, null, (OptimizelyClient client) -> {
+            Variation variation2 = client.activate("app_redesign", userId);
+            Log.d("optimizely", "variation " + variation2);
+        });
 
         Button trackEventButton = findViewById(R.id.trackEventButton);
         trackEventButton.setOnClickListener(new View.OnClickListener() {
@@ -37,12 +58,11 @@ public class MainActivity extends AppCompatActivity {
                 eventValue.put(AFInAppEventParameterName.CONTENT_TYPE, "category_a");
                 eventValue.put(AFInAppEventParameterName.CONTENT_ID, "1234567");
                 eventValue.put(AFInAppEventParameterName.CURRENCY, "USD");
+                eventValue.put(AFInAppEventParameterName.CURRENCY, "USD");
                 AppsFlyerLib.getInstance().trackEvent(getApplicationContext(), AFInAppEventType.PURCHASE, eventValue);
             }
         });
     }
-
-
 
     /*** Ignore - used to display install data ***/
     public void SetInstallDataText(){
